1848540a0f606344b8706a02706956553ef51d87
Mario J. Chan Zurita <56327994+MarioJChanZurita@users.noreply.github.com>
2021-01-20 18:59:14 -0600

    hola

---
+++ DomoticProject/src/test/java/com/domotic/controller/AppTest.java
@@ -0,0 +1,20 @@
+package com.domotic.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
